e7ab67c6f7edab253f2ef326f35bb9194974b5de
Volkov <volkovai@nih.gov>
2022-06-24 09:55:28 -0400

    Fix codesign issue with Qt on MAC

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,15 +1,24 @@
+APPNAME="ConeDetectionML"
 VERSION="1.1.2"
 rm -rf build/
 rm -rf dist/
 pyinstaller --clean --noconfirm build-app-dir.spec
-rm -rf dist/ConeDetectionML/
-hdiutil create "ConeDetectionML-Darwin0.dmg" -format UDRW -ov -volname "ConeDetectionML" -fs HFS+ -srcfolder "dist/" -attach
-ln -s /Applications /Volumes/ConeDetectionML/Applications
-mkdir /Volumes/ConeDetectionML/.background
-cp MacOS/DMGbackground.tif /Volumes/ConeDetectionML/.background/background.tif
-osascript MacOS/DMGSetup.scpt ConeDetectionML
-hdiutil detach /Volumes/ConeDetectionML
-#python MacOS/licenseDMG.py ConeDetectionML-Darwin0.dmg Help/License.txt
-hdiutil convert ConeDetectionML-Darwin0.dmg -format UDZO -o "dist/ConeDetectionML-${VERSION}-Darwin.dmg"
-rm ConeDetectionML-Darwin0.dmg
-dmg-license MacOS/dmg-license-spec.json "dist/ConeDetectionML-${VERSION}-Darwin.dmg"
+rm -rf "dist/${APPNAME}/"
+cd "dist/${APPNAME}.app/Contents/MacOS"
+mv PyQt5/ ../Resources/
+ln -s ../Resources/PyQt5 .
+cd ../Resources/
+ln -s ../MacOS/Qt* .
+cd ../../../..
+codesign --force --deep --verbose -s - "dist/${APPNAME}.app"
+codesign --verify --verbose --strict "dist/${APPNAME}.app"
+hdiutil create "${APPNAME}-Darwin0.dmg" -format UDRW -ov -volname "${APPNAME}" -fs HFS+ -srcfolder "dist/" -attach
+ln -s /Applications "/Volumes/${APPNAME}/Applications"
+mkdir "/Volumes/${APPNAME}/.background"
+cp MacOS/DMGbackground.tif "/Volumes/${APPNAME}/.background/background.tif"
+osascript MacOS/DMGSetup.scpt "${APPNAME}"
+hdiutil detach "/Volumes/${APPNAME}"
+#python MacOS/licenseDMG.py ${APPNAME}-Darwin0.dmg Help/License.txt
+hdiutil convert "${APPNAME}-Darwin0.dmg" -format UDZO -o "dist/${APPNAME}-${VERSION}-Darwin.dmg"
+rm "${APPNAME}-Darwin0.dmg"
+dmg-license MacOS/dmg-license-spec.json "dist/${APPNAME}-${VERSION}-Darwin.dmg"
