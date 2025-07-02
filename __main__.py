5966194ddfa9e03de392124eb80430fea5abc626
volkovaiNIH <volkovai@nih.gov>
2025-07-02 14:51:33 -0400

    Ctrl->Alt
    Ctrl+mouse -> Alt+Mouse override for deleting markers in Add/Move mouse mode.

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import AOConfig as cfg
 
 cfg.APP_NAME = 'Cone Detection (ML)'
-cfg.APP_VERSION = '1.2.0 (2025-05-30)'
+cfg.APP_VERSION = '1.2.0 (2025-07-02)'
 
 if __name__ == '__main__':
     try:
